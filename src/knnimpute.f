98318faf91fc53d97a87c78d435877c9e404fbfb
Balasubramanian Narasimhan <naras@stat.Stanford.EDU>
2007-04-16 18:10:03 +0000

    Fixed fortran comment char issues
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/impute@23928 bc3139a8-67e5-0310-9ffc-ced21a209358

--- src/knnimpute.f
+++ src/knnimpute.f
@@ -1,9 +1,9 @@
       subroutine knnimp(x,ximp,p,n,imiss,irmiss,kn,workp,workn,iworkp,
-&     iworkn)
+     &iworkn)
       integer p,n,i,j,k,kn,m
       integer imiss(p,n),irmiss(p)
       double precision x(p,n),ximp(p,n)
-      integer iworkp(p),iworkn(n), jj
+      integer iworkp(p),iworkn(n)
       double precision workp(p),workn(n)
       m=kn+1
       do 23000 i=1,p
@@ -123,9 +123,9 @@
 
 
       subroutine twomis(x,p,n,imiss,x0,imiss0,maxit,eps,istart,clust, 
-&     nsize,dist,ratio,iter,iworkp,iworkn)
+     &nsize,dist,ratio,iter,iworkp,iworkn)
       integer p,n,imiss(p,n),imiss0(n,2),maxit,istart(2),clust(p,2),
-&     iworkp(p)
+     &iworkp(p)
       double precision x(p,n),x0(n,2),eps,dist(p,2)
       integer nsize(2),iworkn(n)
       integer iter,imax
